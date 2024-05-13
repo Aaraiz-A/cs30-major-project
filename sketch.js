27b9463f4797135208ed07443256d2a8487c1d92
Aaraiz-A <161645440+Aaraiz-A@users.noreply.github.com>
2024-05-13 14:11:31 -0600

    Started the project
    Made a barebones demo that involves gravity

--- sketch.js
+++ sketch.js
@@ -1,12 +1,43 @@
-let floor, ball;
+// Aaraiz Afridi
+// CS30 Major Project
+
+let player;
+let platform;
+const WORLD_GRAVITY = 9.8;
 
 function setup() {
-  new Canvas(500, 200);
-  world.gravity.y = 10;
+  new Canvas(windowWidth, windowHeight);
+  world.gravity.y = WORLD_GRAVITY;
 
-  //                ([vertex0, vertex1, vertex2, ...])
-  floor = new Sprite([[20, 60], [200, 140], [450, 180]]);
-  floor.collider = "static";
+  mainCharacter();
+  thePlatform();
 
-  ball = new Sprite(40, 0, 20);
+}
+
+function draw() {
+  background(255);
+  theCamera();
+  theJump();
+}
+
+function mainCharacter() {
+  player = new Sprite(width/2, height/2, 20);
+  player.color = "green";
+}
+
+function thePlatform() {
+  platform = new Sprite(width/2, height/2 + 50, 100, 20, "static");
+  platform.color = "red";
+}
+
+function theJump() {
+  if (kb.presses("w")) {
+    player.vel.y = -4;
+  }
+
+}
+
+function theCamera() {
+  camera.x = player.x;
+  camera.y = player.y;
 }
