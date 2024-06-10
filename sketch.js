e3ed196f4cdc9a6ac849d37fff68c22c67301054
Aaraiz-A <161645440+Aaraiz-A@users.noreply.github.com>
2024-06-10 14:11:51 -0600

    added checkpoints
    I am trying to implement checkpoints in the game. Right now it is buggy, but I will try to fix it.

--- sketch.js
+++ sketch.js
@@ -10,10 +10,11 @@ const ENEMY1_SPEED = 3;
 let canJump = true;
 let bullets = [];
 let platforms = [];
+let checkpoints = [];
 let dist1;
 let playerHealth = 100;
+let playerSpawnPoint;
 let state = "title screen";
-
 
 function setup() {
   new Canvas(windowWidth, windowHeight);
@@ -31,11 +32,13 @@ function draw() {
     theEnemy1();
     theGunCharacteristics();
     thePlatforms();
+    theCheckpoints();
     state = "game on";
   }
   else if (state === "game on") {
     background(155);
     theCamera();
+    drawCheckpoints();
     playerMovement();
     theGunBehaviour();
     shootyShoot();
@@ -44,27 +47,32 @@ function draw() {
     drawHealthBar();
     gameOver();
   }
+  else if (state === "game over") {
+    background(155);
+    text("game over", width/2, height/2);
+  }
 }
 
 function mainCharacter() {
   player = new Sprite(20, height/2, 20);
   player.color = "green";
+  playerSpawnPoint = createVector(20, height/2);
 }
 
 function theEnemy1() {
   let x
   let y;
   let distance;
-  
-  x = random(0, width);
+
+  x = random(1100, width);
   y = random(0, height/2);
   distance = dist(x, y, player.x, player.y);
-  
+
   while (distance < 100) {
     x = random(0, width);
     y = random(0, height/2);
   }
-  
+
   enemy1 = new Sprite(x, y, 20);
   enemy1.color = "black";
 }
@@ -111,43 +119,45 @@ function playerMovement() {
     player.vel.x = 0;
   }
   if (player.y > height) {
-    player.x = 20; 
-    player.y = height/2; 
+    player.x = playerSpawnPoint.x;
+    player.y = playerSpawnPoint.y;
     player.vel.y = 0;
   }
 }
 
 function thePlatforms() {
-let platform1 = new Sprite(20, height/2 + 50, 100, 20, "static");
-platform1.color = "red";
-platforms.push(platform1);
+  //first checkpoint
+  let platform1 = new Sprite(20, height/2 + 50, 100, 20, "static");
+  platform1.color = "red";
+  platforms.push(platform1);
 
-let platform2 = new Sprite(300, height/2, 200, 20, "static");
-platform2.color = "red";
-platforms.push(platform2);
+  let platform2 = new Sprite(300, height/2, 200, 20, "static");
+  platform2.color = "red";
+  platforms.push(platform2);
 
-let platform3 = new Sprite(500, height/2 + 50, 150, 20, "static");
-platform3.color = "red";
-platforms.push(platform3);
+  let platform3 = new Sprite(500, height/2 + 50, 150, 20, "static");
+  platform3.color = "red";
+  platforms.push(platform3);
 
-let platform4 = new Sprite(900, height/2 + 100, 100, 20, "static");
-platform4.color = "red";
-platforms.push(platform4);
+  let platform4 = new Sprite(900, height/2 + 100, 100, 20, "static");
+  platform4.color = "red";
+  platforms.push(platform4);
 
-let platform5 = new Sprite(1000, height/2, 20, 100, "static");
-platform5.color = "red";
-platforms.push(platform5);
+  let platform5 = new Sprite(1000, height/2, 20, 100, "static");
+  platform5.color = "red";
+  platforms.push(platform5);
 
-let platform6 = new Sprite(1100, height/2, 100, 20, "static");
-platform6.color = "red";
-platforms.push(platform6);
+  let platform6 = new Sprite(1100, height/2, 100, 20, "static");
+  platform6.color = "red";
+  platforms.push(platform6);
 }
 
+function theCheckpoints() {
+  let checkpoint1 = new Checkpoint(1170, height/2, 20, 20, "static");
+  checkpoints.push(checkpoint1);
+}
 
 function theCamera() {
-  // camera.x = mouseX;
-  // camera.y = mouseY;
-
   camera.x = player.x;
   camera.y = player.y;
 }
@@ -195,16 +205,16 @@ function drawHealthBar() {
 
 function gameOver() {
   if (playerHealth <= 0) {
-    noLoop();
+    state = "game over";
   }
-} 
+}
 
 function titleScreenText() {
   textAlign(CENTER);
   textFont("Trebuchet MS");
   fill("black");
   textSize(40);
-  text("The Game", width / 2, height / 2);
+  text("CIRCLE ADVENTURES", width / 2, height / 2);
 }
 
 function titleScreenButtonStart() {
@@ -225,4 +235,30 @@ function titleScreenButtonStart() {
     state = "set up";
   };
   startButton.draw();
+}
+
+function drawCheckpoints() {
+  for (let i = 0; i < checkpoints.length; i++) {
+    checkpoints[i].draw();
+    checkpoints[i].checkCollision();
+  }
+}
+
+function Checkpoint(x, y, width, height, state) {
+  this.sprite = new Sprite(x, y, width, height, state);
+  this.sprite.color = "yellow";
+  this.touched = false;
+
+  this.draw = function() {
+    fill(this.sprite.color);
+    rect(this.sprite.x, this.sprite.y, this.sprite.width, this.sprite.height);
+  }
+
+  this.checkCollision = function() {
+    if (player.collides(this.sprite) && this.touched === false) {
+      this.sprite.color = "green";
+      this.touched = true;
+      playerSpawnPoint = createVector(this.sprite.x, this.sprite.y - 50);
+    }
+  }
 }
